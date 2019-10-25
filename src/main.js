8a3993fdf8ce1a41bbeace2c28a4a306f82de1d4
Breno Lemos <brenomiros@gmail.com>
2019-10-24 23:13:57 -0300

    COnfigurando o vuex no projeto

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import "./plugins/vuetify";
 import App from "./App.vue";
 
 import router from "./router";
+import store from "./store/store";
 
 Vue.config.productionTip = false;
 
+// Vue.prototype.$store = store;
+
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount("#app");
