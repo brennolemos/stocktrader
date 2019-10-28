1cdc1ed3167f0c56011f43b7d6eb2edf3a21dc32
Breno Lemos <brenomiros@gmail.com>
2019-10-28 11:48:08 -0300

    Trabalhando com Ações no Portfolio

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -1,9 +1,16 @@
 <template>
-  <h1>Portfolio</h1>
+  <div>
+    <h1>Portfolio</h1>
+    <Stock />
+  </div>
 </template>
 
 <script>
-export default {};
+import Stock from "./Stock";
+export default {
+  name: "Portfolio",
+  components: { Stock }
+};
 </script>
 
 <style>
